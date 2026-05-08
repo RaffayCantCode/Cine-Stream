5b3b238ec70d0d1da7e58c460eadcbfd88de7652
Raffay <asifraffy@gmail.com>
2026-05-09 02:36:53 +0500

    files!

--- lib/replit-auth-web/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMeta {
-  readonly env: {
-    readonly BASE_URL: string;
-    readonly MODE: string;
-    readonly DEV: boolean;
-    readonly PROD: boolean;
-    [key: string]: string | boolean | undefined;
-  };
-}
